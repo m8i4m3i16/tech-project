02f94d671647cee5de54c4627f20244e8f443c72
Hayley <m8i4m3i16@gmail.com>
2024-04-12 21:20:56 +0800

    update search-bar UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,6 @@
 import Image from "next/image";
-// import NavBar from "./components/navbar";
 import SearchBar from "./components/search-bar";
 
-// import { useClient } from "next/client";
-// import { useEffect, useState } from "react";
-
-// import { ThemeProvider, createTheme } from "@mui/material/styles";
-// import Button from "@mui/material/Button";
-
 export default function Home() {
-  // const client = useClient();
-  // const theme = createTheme();
-
-  // useEffect(() => {
-  //   setTheme(createTheme());
-  // }, []);
-
-  // if (!theme) {
-  //   return null;
-  // }
-
-  return (
-    // <main>
-    //   <h1>hello world</h1>
-    // </main>
-    // <NavBar />
-    <SearchBar />
-  );
+  return <SearchBar />;
 }
