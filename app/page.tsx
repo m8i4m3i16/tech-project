869f9a0fb1e335286aacf7a6d5b17a478b66d42c
Hayley <m8i4m3i16@gmail.com>
2024-04-14 12:25:54 +0800

    succeed

--- app/page.tsx
+++ app/page.tsx
@@ -6,121 +6,100 @@ import SideBar from "./components/side-bar";
 import Section from "./components/section";
 
 export default function Home() {
-  const [apiData, setApiData] = useState([]);
-  // const [taiwanStockData, setTaiwanStockData] = useState([]);
-  // const [usStockData, setUsStockData] = useState([]);
-  const [stockNames, setStockNames] = useState([]);
-  const [stockIds, setStockIds] = useState([]);
+  const [taiwanStockData, setTaiwanStockData] = useState([]);
+  const [usStockData, setUsStockData] = useState([]);
+  const [taiwanStockNames, setTaiwanStockNames] = useState([]);
+  const [taiwanStockIds, setTaiwanStockIds] = useState([]);
+  const [usStockNames, setUsStockNames] = useState([]);
+  const [usStockIds, setUsStockIds] = useState([]);
   const [searchResults, setSearchResults] = useState([]);
 
-  //更新搜尋結果狀態
-  const handleSearchResults = (results) => {
-    console.log("搜尋結果", results);
-    setSearchResults(results);
-  };
-
-  // useEffect(() => {
-  //   const fetchTaiwanStockData = async () => {
-  //     try {
-  //       const response = await fetch(
-  //         "https://api.finmindtrade.com/api/v4/data?dataset=TaiwanStockInfo"
-  //       );
-  //       if (!response.ok) {
-  //         throw new Error("Failed to fetch Taiwan stock data");
-  //       }
-  //       const jsonData = await response.json();
-  //       setTaiwanStockData(jsonData.data || []);
-
-  //       if (jsonData && jsonData.data) {
-  //         const names = jsonData.data.map((i) => i.stock_name);
-  //         const ids = jsonData.data.map((i) => i.stock_id);
-  //         setStockNames(names);
-  //         setStockIds(ids);
-  //       }
-  //     } catch (error) {
-  //       console.error("Error fetching Taiwan stock data:", error);
-  //     }
-  //   };
-
-  //   const fetchUsStockData = async () => {
-  //     try {
-  //       const response = await fetch(
-  //         "https://api.finmindtrade.com/api/v4/data?dataset=USStockInfo"
-  //       );
-  //       if (!response.ok) {
-  //         throw new Error("Failed to fetch US stock data");
-  //       }
-  //       const jsonData = await response.json();
-  //       setUsStockData(jsonData.data || []);
-
-  //       if (jsonData && jsonData.data) {
-  //         const names = jsonData.data.map((i) => i.stock_name);
-  //         const ids = jsonData.data.map((i) => i.stock_id);
-  //         setStockNames(names);
-  //         setStockIds(ids);
-  //       }
-  //     } catch (error) {
-  //       console.error("Error fetching US stock data:", error);
-  //     }
-  //   };
-
-  //   fetchTaiwanStockData();
-  //   fetchUsStockData();
-  // }, []);
-
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchTaiwanStockData = async () => {
       try {
         const response = await fetch(
           "https://api.finmindtrade.com/api/v4/data?dataset=TaiwanStockInfo"
         );
         if (!response.ok) {
-          throw new Error("Failed to fetch data");
+          throw new Error("Failed to fetch Taiwan stock data");
         }
         const jsonData = await response.json();
-        setApiData(jsonData);
+        setTaiwanStockData(jsonData.data || []);
 
         if (jsonData && jsonData.data) {
           const names = jsonData.data.map((i) => i.stock_name);
           const ids = jsonData.data.map((i) => i.stock_id);
-          setStockNames(names);
-          setStockIds(ids);
+          setTaiwanStockNames(names);
+          setTaiwanStockIds(ids);
         }
       } catch (error) {
-        console.error("Error fetching data:", error);
+        console.error("Error fetching Taiwan stock data:", error);
       }
     };
-    fetchData();
+
+    const fetchUsStockData = async () => {
+      try {
+        const response = await fetch(
+          "https://api.finmindtrade.com/api/v4/data?dataset=USStockInfo"
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch US stock data");
+        }
+        const jsonData = await response.json();
+        setUsStockData(jsonData.data || []);
+
+        if (jsonData && jsonData.data) {
+          const names = jsonData.data.map((i) => i.stock_name);
+          const ids = jsonData.data.map((i) => i.stock_id);
+          setUsStockNames(names);
+          setUsStockIds(ids);
+        }
+      } catch (error) {
+        console.error("Error fetching US stock data:", error);
+      }
+    };
+
+    fetchTaiwanStockData();
+    fetchUsStockData();
   }, []);
 
-  //搜尋結果中的股票名稱或股票代碼，轉換成對應的股票代碼和股票名稱
+  const handleSearchResults = (results) => {
+    setSearchResults(results);
+  };
+
   const getStockInfoFromResults = () => {
     const filteredIds = [];
     const filteredNames = [];
 
     searchResults.forEach((result) => {
-      //檢查結果是否為股票代碼
-      const isId = stockIds.includes(result);
+      const isId =
+        taiwanStockIds.includes(result) || usStockIds.includes(result);
       if (isId) {
         filteredIds.push(result);
       } else {
-        //如果不是股票代碼，則檢查是否為股票名稱
-        const index = stockNames.indexOf(result);
+        const index = taiwanStockNames.indexOf(result);
         if (index !== -1) {
-          //如果找到對應的股票名稱，則將其對應的股票代碼添加到 filteredIds 陣列中
-          filteredIds.push(stockIds[index]);
+          filteredIds.push(taiwanStockIds[index]);
           filteredNames.push(result);
+        } else {
+          const usIndex = usStockNames.indexOf(result);
+          if (usIndex !== -1) {
+            filteredIds.push(usStockIds[usIndex]);
+            filteredNames.push(result);
+          }
         }
       }
     });
     return { stockIds: filteredIds, stockNames: filteredNames };
   };
 
+  const { stockIds, stockNames } = getStockInfoFromResults();
+
   return (
     <body style={{ backgroundColor: "#f2f2f2" }}>
       <SearchBar
-        stockIds={stockIds}
-        stockNames={stockNames}
+        stockIds={[...taiwanStockIds, ...usStockIds]}
+        stockNames={[...taiwanStockNames, ...usStockNames]}
         handleSearchResults={handleSearchResults}
       />
       <div
@@ -132,8 +111,8 @@ export default function Home() {
       >
         <SideBar style={{ flexDirection: "column" }} />
         <Section
-          stockIds={stockIds}
-          stockNames={stockNames}
+          stockIds={[...taiwanStockIds, ...usStockIds]}
+          stockNames={[...taiwanStockNames, ...usStockNames]}
           searchResults={searchResults}
         />
       </div>
